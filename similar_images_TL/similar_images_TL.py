28a8887ed5f0260c13d552cc04fc9b76e50632cd
david <davidtnfsh@gmail.com>
2018-09-04 20:37:56 +0800

    refactor

--- similar_images_TL/similar_images_TL.py
+++ similar_images_TL/similar_images_TL.py
@@ -1,6 +1,6 @@
 """
 
- similar_images_TL.py  (author: Anson Wong / git: ankonzoid)
+ similar_images_tl.py  (author: Anson Wong / Chang Tai Wei)
 
  We find similar images in a database by using transfer learning
  via a pre-trained VGG image classifier. We plot the 5 most similar
@@ -8,23 +8,32 @@
  our image feature vectors.
 
 """
-import sys, os
-import numpy as np
-from keras.preprocessing import image
-from keras.applications.inception_resnet_v2 import InceptionResNetV2
-from keras.applications.inception_resnet_v2 import preprocess_input, decode_predictions
-from keras.models import Model
+import os
+from pathlib import Path
 
+import numpy as np
+
+import keras
+import tensorflow as tf
+from keras.applications.inception_resnet_v2 import (InceptionResNetV2,
+                                                    preprocess_input)
+from keras.preprocessing import image
+from src.kNN import kNN
 from src.plot_utils import plot_query_answer
 from src.sort_utils import find_topk_unique
-from src.kNN import kNN
 from src.tSNE import plot_tsne
 
-from pathlib import Path
-import tqdm
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+CONFIG = tf.ConfigProto()
+CONFIG.gpu_options.allow_growth = True
+SESS = tf.Session(config=CONFIG)
+keras.backend.set_session(SESS)
 
 
 def main():
+    '''
+    A script which can visualize picture's embedding
+    '''
     # ================================================
     # Load pre-trained model and remove higher level layers
     # ================================================
@@ -38,32 +47,30 @@ def main():
     img_list = []
     path = "db"
     print("Reading images from '{}' directory...\n".format(path))
-    for index, f in enumerate(Path(path).iterdir()):
-        if index == 50:
-            break
+    for file_path in Path(path).iterdir():
         # Process filename
-        head, ext = f.name, f.suffix
+        head, ext = file_path.name, file_path.suffix
         if ext.lower() not in [".jpg", ".jpeg"]:
             continue
 
         # Read image file
-        img = image.load_img(f, target_size=(224, 224))  # load
+        img = image.load_img(str(file_path), target_size=(224, 224))  # load
         imgs.append(np.array(img))  # image
         filename_heads.append(head)  # filename head
 
         # Pre-process for model input
         img = image.img_to_array(img)  # convert to array
         img = np.expand_dims(img, axis=0)
-        if len(img_list) > 0:
+        if len(img_list) > 1:
             img_list = np.concatenate((img_list, img))
         else:
             img_list = img
 
     img_list = preprocess_input(img_list)
-    X = model.predict(img_list).reshape(len(img_list), -1)
+    predicts = model.predict(img_list).reshape(len(img_list), -1)
     imgs = np.array(imgs)  # images
     print("imgs.shape = {}".format(imgs.shape))
-    print("X_features.shape = {}\n".format(X.shape))
+    print("X_features.shape = {}\n".format(predicts.shape))
 
     # ===========================
     # Find k-nearest images to each image
@@ -71,7 +78,7 @@ def main():
     n_neighbours = 5 + 1  # +1 as itself is most similar
     knn = kNN()  # kNN model
     knn.compile(n_neighbors=n_neighbours, algorithm="brute", metric="cosine")
-    knn.fit(X)
+    knn.fit(predicts)
 
     # ==================================================
     # Plot recommendations for each image in database
@@ -82,10 +89,10 @@ def main():
     n_imgs = len(imgs)
     ypixels, xpixels = imgs[0].shape[0], imgs[0].shape[1]
     for ind_query in range(n_imgs):
-
         # Find top-k closest image feature vectors to each vector
-        print("[{}/{}] Plotting similar image recommendations for: {}".format(ind_query+1, n_imgs, filename_heads[ind_query]))
-        distances, indices = knn.predict(np.array([X[ind_query]]))
+        print("[{}/{}] Plotting similar image recommendations \
+        for: {}".format(ind_query+1, n_imgs, filename_heads[ind_query]))
+        distances, indices = knn.predict(np.array([predicts[ind_query]]))
         distances = distances.flatten()
         indices = indices.flatten()
         indices, distances = find_topk_unique(indices, distances, n_neighbours)
@@ -96,7 +103,7 @@ def main():
         x_answer_plot = imgs[indices].reshape((-1, ypixels, xpixels, 3))
         plot_query_answer(x_query=x_query_plot,
                           x_answer=x_answer_plot[1:],  # remove itself
-                          filename=rec_filename)
+                          filename=str(rec_filename))
 
     # ===========================
     # Plot tSNE
@@ -106,7 +113,7 @@ def main():
         output_tsne_dir.mkdir()
     tsne_filename = Path(output_tsne_dir, "tsne.png")
     print("Plotting tSNE to {}...".format(tsne_filename))
-    plot_tsne(imgs, X, tsne_filename)
+    plot_tsne(imgs, predicts, str(tsne_filename))
 
 # Driver
 if __name__ == "__main__":
